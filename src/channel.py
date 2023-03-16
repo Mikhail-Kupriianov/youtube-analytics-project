7c5d3cd545ae324e334f63bb29356b09abf47c7e
Mkupr <mkupr@outlook.com>
2023-03-17 00:38:55 +0300

    edit channel.pyel

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+import os
+from googleapiclient.discovery import build
+import json
 
 
 class Channel:
@@ -5,8 +8,17 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.dict_to_print = None
+        self.channel_id = channel_id
+
+    def printj(self, dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        self.dict_to_print = dict_to_print
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key: str = os.getenv('API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.printj(channel)
