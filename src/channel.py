17e6ae38ade91b61ad98917e7f2b210552ebdc11
Mkupr <mkupr@outlook.com>
2023-03-22 18:04:47 +0300

    edit channel.py

--- src/channel.py
+++ src/channel.py
@@ -5,6 +5,8 @@ import json
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -18,7 +20,7 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        api_key: str = os.getenv('API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        # api_key: str = os.getenv('API_KEY')
+        # youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         self.printj(channel)
